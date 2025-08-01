ca4f6557b0bec363e5e31782b383690e2f41d8ec
sadiana <dev.sadiana@gmail.com>
2025-08-01 19:51:56 -0300

    feat: building http response body

--- src/main/java/br/com/one/conversorDeMoedas/service/ConsumoApi.java
+++ src/main/java/br/com/one/conversorDeMoedas/service/ConsumoApi.java
@@ -13,4 +13,18 @@ public class ConsumoApi {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
+        HttpResponse<String> response = null;
+
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        return json;
+    }
 }
