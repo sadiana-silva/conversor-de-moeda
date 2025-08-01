d600d25fb75b9ede58dea38575384f5f6bdd82c4
sadiana <dev.sadiana@gmail.com>
2025-08-01 19:58:05 -0300

    collapsing catch blocks

--- src/main/java/br/com/one/conversorDeMoedas/service/ConsumoApi.java
+++ src/main/java/br/com/one/conversorDeMoedas/service/ConsumoApi.java
@@ -18,9 +18,7 @@ public class ConsumoApi {
         try {
             response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
 
